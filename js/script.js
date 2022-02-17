aa26133d3cb84b934aecb058b9d8dda75a645174
Torikul <toriqulislam142@gmail.com>
2022-02-17 13:32:01 +0600

    Fixed some errors

--- js/script.js
+++ js/script.js
@@ -1,52 +1,52 @@
-document.getElementById('calc-button').addEventListener('click' ,function(){
-    const incomeInput = document.getElementById('income-input');
-    const incomeInputValue = parseFloat( incomeInput.value);
-    const foodInput = document.getElementById('food-input');
-    const foodInputValue = parseFloat(foodInput.value) ;
-    const rentInput = document.getElementById('rent-input');
-    const rentInputValue = parseFloat(rentInput.value);
-    const clothInput = document.getElementById('clothe-input')
-    const clothInputValue = parseFloat( clothInput.value);
-    const totalValues = foodInputValue + rentInputValue + clothInputValue;
+//fucntion  of input values 
+function getValue(inputs) {
+    const input = document.getElementById(inputs);
+    const inputValue = parseFloat(input.value);
+    return inputValue
+}
+document.getElementById('calc-button').addEventListener('click', function () {
 
-    if(incomeInputValue > 0 && totalValues < incomeInputValue  ){
+    const incomeInputValue = getValue('income-input');
+    const foodInputValue = getValue('food-input');
+    const rentInputValue = getValue('rent-input');
+    const clothInputValue = getValue('clothe-input')
+   if(foodInputValue >= 0 && rentInputValue >= 0 && clothInputValue >= 0){
+    var totalValues = foodInputValue + rentInputValue + clothInputValue;
+   }
+    if (incomeInputValue > 0 && totalValues < incomeInputValue) {
         var totalExpenses = document.getElementById('total-expenses');
-    totalExpenses.innerText = totalValues;
+        totalExpenses.innerText = totalValues;
+    } else {
+        alert('input valid numer');
     }
-    else{
-       alert('input valid numer');   
-}
-if(incomeInput.value > 0 && totalValues < incomeInputValue){
-    const remainingBalance = incomeInputValue - totalValues;
-const mainBalance = document.getElementById('main-balance');
-mainBalance.innerText = remainingBalance;
-}
+    if (incomeInputValue > 0 && totalValues < incomeInputValue) {
+        const remainingBalance = incomeInputValue - totalValues;
+        const mainBalance = document.getElementById('main-balance');
+        mainBalance.innerText = remainingBalance;
+    }
 })
 
 // add even listener of Save button
 
-document.getElementById('save-button').addEventListener('click' , function(){
-    const incomeInput = document.getElementById('income-input');
-    var incomeInputValue = parseFloat( incomeInput.value);   
-    const mainBalance = document.getElementById('main-balance');  
-    const latestMainBalance = parseFloat(mainBalance.innerText);
-    const parsentInput = document.getElementById('parsent-input');
-    const parsentInputValue = parseFloat( parsentInput.value);
-  console.log(parsentInputValue)
-    if(parsentInput.value <= 100 && latestMainBalance != 0 ){
-        console.log(parsentInputValue)
-    const save = parsentInputValue /100;
-    var getSavingBalance = save * incomeInputValue  ; 
-    const saveInput = document.getElementById('save-amount');
-    saveInput.innerText = getSavingBalance;
-    }
-    else{
-        alert('invalid')
-    }
+document.getElementById('save-button').addEventListener('click', function () {
 
-    const lastbalance = latestMainBalance - parseFloat(getSavingBalance)
-    const remainingBalance = document.getElementById('remaining-balance');
-    remainingBalance.innerText = lastbalance;
+    const incomeInputValue = getValue('income-input');
+    const mainBalance = document.getElementById('main-balance');
+    const latestMainBalance = parseFloat(mainBalance.innerText);
+    const parsentInputValue = getValue('parsent-input');
+
+    if (parsentInputValue <= 100 && latestMainBalance != 0) {
+        if (mainBalance.innerText > parsentInputValue) {
+            const save = parsentInputValue / 100;
+            var getSavingBalance = save * incomeInputValue;
+        }
+       else if (getSavingBalance > 0 && getSavingBalance < latestMainBalance) {
+            const saveInput = document.getElementById('save-amount');
+            saveInput.innerText = getSavingBalance.toFixed(2);
+            const lastbalance = latestMainBalance - parseFloat(getSavingBalance)
+            const remainingBalance = document.getElementById('remaining-balance');
+            remainingBalance.innerText = lastbalance.toFixed(2);
+        }
+    }
 
 })
-
