5202f053ee42cbaf85754e616f3e184c586c0a78
Torikul <toriqulislam142@gmail.com>
2022-02-17 12:32:35 +0600

    some fixed of parsenteage calc

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ document.getElementById('calc-button').addEventListener('click' ,function(){
     const clothInput = document.getElementById('clothe-input')
     const clothInputValue = parseFloat( clothInput.value);
     const totalValues = foodInputValue + rentInputValue + clothInputValue;
+
     if(incomeInputValue > 0 && totalValues < incomeInputValue  ){
         var totalExpenses = document.getElementById('total-expenses');
     totalExpenses.innerText = totalValues;
@@ -25,14 +26,24 @@ mainBalance.innerText = remainingBalance;
 // add even listener of Save button
 
 document.getElementById('save-button').addEventListener('click' , function(){
+    const incomeInput = document.getElementById('income-input');
+    var incomeInputValue = parseFloat( incomeInput.value);   
     const mainBalance = document.getElementById('main-balance');  
     const latestMainBalance = parseFloat(mainBalance.innerText);
     const parsentInput = document.getElementById('parsent-input');
     const parsentInputValue = parseFloat( parsentInput.value);
-    const save = parsentInputValue /latestMainBalance;
-    const getSavingBalance = save * 100 ; 
+  console.log(parsentInputValue)
+    if(parsentInput.value <= 100 && latestMainBalance != 0 ){
+        console.log(parsentInputValue)
+    const save = parsentInputValue /100;
+    var getSavingBalance = save * incomeInputValue  ; 
     const saveInput = document.getElementById('save-amount');
     saveInput.innerText = getSavingBalance;
+    }
+    else{
+        alert('invalid')
+    }
+
     const lastbalance = latestMainBalance - parseFloat(getSavingBalance)
     const remainingBalance = document.getElementById('remaining-balance');
     remainingBalance.innerText = lastbalance;
